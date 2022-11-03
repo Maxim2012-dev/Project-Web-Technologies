a5760981658d10d67f6884b83c1c78eff6ef7832
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2022-11-03 19:54:08 +0100

    official-project-setup

---
+++ event-guard/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header-container">
+    <div class="title-container">
+      <h1 id="title">EventGuard</h1>
+    </div>
+    <div class="nav-container">
+      <a href="login.html">Organisator</a>
+      <a href="login.html">Verhuurder</a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style>
+  #title {
+    font-family: 'Ubuntu', sans-serif;
+    padding: 20px;
+    font-size: 40px;
+  }
+
+  body {
+      margin: 0;
+      min-height: 100%;
+      background-color: lightgray;
+  }
+
+  .header-container {
+      background-color: white;
+  }
+
+  .nav-container {
+      font-family: 'Ubuntu', sans-serif;
+      display: flex;
+      justify-content: flex-end;
+  }
+
+  .nav-container a {
+      text-decoration: none;
+      padding: 10px 20px;
+      font-weight: bold;
+      font-family: 'Ubuntu', Helvetica, sans-serif;
+      font-size: 20px;
+  }
+</style>
