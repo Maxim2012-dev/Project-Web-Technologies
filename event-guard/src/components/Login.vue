c43bb6218acc0d3303ca5a71aca7bccfb6abc4c5
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2022-11-06 16:27:07 +0100

    Login-wip

---
+++ event-guard/src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="mdc-text-field">
+      <input type="text" id="my-text-field" class="mdc-text-field__input first-name-input">
+      <label class="mdc-floating-label" for="my-text-field">First name</label>
+      <div class="mdc-line-ripple"></div>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: 'Login',
+    }
+  </script>
+  
+  <style>
+    html, body {  
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 32px;
+    }
+
+    .mdc-text-field {
+        margin-bottom: 24px;
+        flex-shrink: 0;
+    }
+  </style>
+  
