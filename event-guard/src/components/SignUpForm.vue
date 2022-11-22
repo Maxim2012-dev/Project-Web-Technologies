5c0a676f32c488117d9e910c79fbeb8363ef0e89
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2022-11-22 20:46:42 +0100

    front side register validation

--- event-guard/src/components/SignUpForm.vue
+++ event-guard/src/components/SignUpForm.vue
@@ -1,27 +1,33 @@
 <template>
     <v-app>
-        <v-form class="form">
+        <v-form class="form" v-model="formValidity">
             <h1>Sign Up Today!</h1>
             <h3 class="register-message">{{ register_type }} Registration</h3>
-            <v-text-field outlined v-model="name" label="Name" id="first-name" prepend-inner-icon="mdi-account-circle-outline">
+            <v-text-field outlined v-model="name" label="Name" prepend-inner-icon="mdi-account-circle-outline" required
+            :rules="nameRules">
             </v-text-field>
-            <v-text-field outlined v-model="telnr" label="Telephone number" id="tel-number" prepend-inner-icon="mdi-phone-outline">
+            <v-text-field outlined v-model="telnr" label="Telephone number" prepend-inner-icon="mdi-phone-outline" required
+            :rules="telRules">
             </v-text-field>
-            <v-text-field outlined v-model="email" label="E-mail address" id="e-mail" prepend-inner-icon="mdi-email-outline">
+            <v-text-field outlined v-model="email" label="E-mail address" prepend-inner-icon="mdi-email-outline" required
+            :rules="emailRules">
             </v-text-field>
-            <v-text-field outlined v-model="username" label="Username" id="username" prepend-inner-icon="mdi-account-circle-outline">
+            <v-text-field outlined v-model="username" label="Username" prepend-inner-icon="mdi-account-circle-outline" required
+            :rules="usernameRules">
             </v-text-field>
-            <v-text-field outlined v-model="password" label="Password" id="password" prepend-inner-icon="mdi-lock-outline">
+            <v-text-field outlined v-model="password" label="Password" prepend-inner-icon="mdi-lock-outline" required
+            :rules="passwordRules">
             </v-text-field>
             <div>
                 <v-btn class="btn" text @click="login_event_handler">Log in</v-btn>
-                <v-btn class="btn" depressed @click="add_user">Register</v-btn>
+                <v-btn class="btn" depressed :disabled="!formValidity" @click="add_user">Register</v-btn>
             </div>
         </v-form>
     </v-app>
 </template>
 
 <script>
+/* eslint-disable */
 import axios from "axios";
 export default {
     name: 'SignUpForm',
@@ -30,7 +36,27 @@ export default {
         telnr: "",
         email: "",
         username: "",
-        password: ""
+        password: "",
+        formValidity: false,
+        nameRules: [
+            v => !!v || 'Name is required.'
+        ],
+        telRules: [
+            v => !!v || 'Telephone number is required.',
+            v => /[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/.test(v) || 'Number should be valid.'
+        ],
+        emailRules: [
+            v => !!v || 'Email address is required.',
+            v => v.includes('@') || 'Email should be valid.'
+        ],
+        usernameRules: [
+            v => !!v || 'Username is required.',
+            v => v.length >= 6 || 'Username should be longer than 6 characters'
+        ],
+        passwordRules: [
+            v => !!v || 'Password is required.',
+            v => /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/.test(v) || 'a lowercase letter, number, special character and uppercase letter required.'
+        ]
     }),
     props: ['register_type'],
     methods: {
@@ -38,15 +64,17 @@ export default {
             this.$emit('login-event', this.register_type)
         },
         add_user() {
-            axios.post("http://localhost:3000/user/add", {
-                name: this.name,
-                telnr: this.telnr,
-                email: this.email,
-                username: this.username,
-                password: this.password
-            }).then(response => {
-                console.log(response.data)
-            })
+            if (this.formValidity) {
+                axios.post("http://localhost:3000/user/add", {
+                    name: this.name,
+                    telnr: this.telnr,
+                    email: this.email,
+                    username: this.username,
+                    password: this.password
+                }).then(response => {
+                   console.log(response.data)
+                })
+            }
         }
     }
 }
