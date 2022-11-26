477f40447a19494bae11d54c769bd3bb0511193f
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2022-11-26 10:13:26 +0100

    temp Home Page

--- event-guard/src/App.vue
+++ event-guard/src/App.vue
@@ -35,7 +35,7 @@ export default {
 
 <style>
 html {
-    scroll-behavior: smooth;
+  scroll-behavior: smooth;
 }
 
 #title {
--- event-guard/src/components/HomePage.vue
+++ event-guard/src/components/HomePage.vue
@@ -1,6 +1,15 @@
 <template>
     <v-app>
-      
+      <div class="section-1">
+        <div class="text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid eum quaerat iste officiis, 
+          adipisci vel aspernatur illo, perferendis nihil, id iusto recusandae accusantium. 
+          Nostrum ipsa reiciendis nesciunt blanditiis. Quia, earum.
+        </div>
+        <div class="image">
+          <img src="../assets/img1.jpg" class="img1" alt="event equipment">
+        </div>
+      </div>
     </v-app>
   </template>
     
@@ -11,5 +20,30 @@
   </script>
     
   <style>
+  .section-1 {
+    display: flex;
+    justify-content: center;
+    margin: 80px;
+    background-color: gainsboro;
+    border: 2px solid red;
+  }
 
+  .text {
+    border: 2px solid green;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 2vw;
+    width: 50%;
+    margin: 25px 10px;
+  }
+
+  .image {
+    border: 2px solid green;
+    margin: 25px 10px;
+    width: 50%;
+  }
+
+  .img1 {
+    width: 100%;
+    height: auto;
+  }
   </style>
