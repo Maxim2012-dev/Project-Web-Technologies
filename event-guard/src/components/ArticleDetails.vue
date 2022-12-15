84ecbafd04d07cd0cc3b872080b781424e1d22c7
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2022-12-15 20:54:43 +0100

    Update ArticleDetails.vue

--- event-guard/src/components/ArticleDetails.vue
+++ event-guard/src/components/ArticleDetails.vue
@@ -1,43 +1,44 @@
 <template>
-  <h1>Article Details</h1>
-  <h4> Product-ID is {{ this.productId }}</h4>
-  <img src="../assets/koelkast.jpg" alt="koelkast" max-width="500" max-height="500">
-  <h2> € 30.00 </h2>
-  <h3> goedwerkende koelkast met vriesvak </h3>
-  <div id="ammountContainer">
-    <button v-on:click="decrementCounter" id="decrement">-</button>
-    <div id="number">{{ count }}</div>
-    <button v-on:click="incrementCounter" id="increment">+</button>
-  </div>
-  <button id="orderButton"> Add to shopping cart </button> 
-
+  <v-app>
+    <h1>Article Details</h1>
+    <h4> Product-ID is {{ this.productId }}</h4>
+    <img src="../assets/koelkast.jpg" alt="koelkast" max-width="500" max-height="500">
+    <h2> € 30.00 </h2>
+    <h3> goedwerkende koelkast met vriesvak </h3>
+    <div id="ammountContainer">
+      <button v-on:click="decrementCounter" id="decrement">-</button>
+      <div id="number">{{ count }}</div>
+      <button v-on:click="incrementCounter" id="increment">+</button>
+    </div>
+    <button id="orderButton"> Add to shopping cart </button>
+  </v-app>
 </template>
 
 <script>
 export default {
- data() { 
-   return {
-     productId:this.$route.params.id,
-     count: 0
-   } 
- },
- methods: {
-   incrementCounter: function() {
-    //if(this.ammount > this.count){
-     this.count +=1;
+  data() {
+    return {
+      productId: this.$route.params.id,
+      count: 0
+    }
+  },
+  methods: {
+    incrementCounter: function () {
+      //if(this.ammount > this.count){
+      this.count += 1;
     },
-   decrementCounter: function() {
-     if(this.count > 0){
-       this.count -=1;
-     }
-   }
- }
+    decrementCounter: function () {
+      if (this.count > 0) {
+        this.count -= 1;
+      }
+    }
+  }
 }
 
 </script>
 
 <style>
-#ammountContainer{
+#ammountContainer {
   display: flex;
   flex-direction: row;
   align-items: flex-start;
@@ -54,19 +55,19 @@ export default {
   margin: 0 5px;
 }
 
-#increment{
+#increment {
   height: 27px;
   width: 100px;
 
 }
 
-#decrement{
+#decrement {
   height: 27px;
   width: 100px;
- 
+
 }
 
-#orderButton{
+#orderButton {
   margin: 12px;
   height: 30px;
   width: 150px;
