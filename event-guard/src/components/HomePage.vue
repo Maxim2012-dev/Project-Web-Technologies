7e598f24565094602f7d1bd224854206e1302430
Maxim2012-dev <55281306+Maxim2012-dev@users.noreply.github.com>
2022-12-18 21:44:16 +0100

    home layout update

--- event-guard/src/components/HomePage.vue
+++ event-guard/src/components/HomePage.vue
@@ -2,9 +2,8 @@
     <v-app>
       <div class="section">
         <div class="text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid eum quaerat iste officiis, 
-          adipisci vel aspernatur illo, perferendis nihil, id iusto recusandae accusantium. 
-          Nostrum ipsa reiciendis nesciunt blanditiis. Quia, earum.
+          Do you still require a party tent or some specific kind of tent for your events?
+          Join the community and discover multiple companies who can offer you that. 
         </div>
         <div class="image">
           <img src="../assets/img1.jpg" class="img1" alt="event tent">
@@ -15,16 +14,15 @@
           <img src="../assets/img2.jpg" class="img1" alt="light equipment">
         </div>
         <div class="text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid eum quaerat iste officiis, 
-          adipisci vel aspernatur illo, perferendis nihil, id iusto recusandae accusantium. 
-          Nostrum ipsa reiciendis nesciunt blanditiis. Quia, earum.
+          You got everything set up for the biggest event, you have ever organised, but you
+          still need light equipment? Well, this is the place to go...
+          Be inspired by the largest range of light installatinos offered by various providers. 
         </div>
       </div>
       <div class="section">
         <div class="text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid eum quaerat iste officiis, 
-          adipisci vel aspernatur illo, perferendis nihil, id iusto recusandae accusantium. 
-          Nostrum ipsa reiciendis nesciunt blanditiis. Quia, earum.
+          Or you might be planning to book a famous artist to perform at your event?
+          Then you should explore the largest range of stage equipment, offered by the community!
         </div>
         <div class="image">
           <img src="../assets/img3.jpg" class="img1" alt="stage equipment">
@@ -44,9 +42,10 @@
     display: flex;
     justify-content: center;
     align-self: center;
-    margin: 50px 0px 50px;
+    padding-top: 50px;
+    margin: 80px 0px 80px;
     width: 80%;
-    background-color: gainsboro;
+    border-top: 3px solid gainsboro;
   }
 
   .text {
