496719d08f661992fbe00726f31e55ca0afc0fae
Kwint3n <116078562+Kwint3n@users.noreply.github.com>
2022-12-20 23:18:23 +0100

    Add files via upload

---
+++ event-guard/src/components/Organiser/RatingTemplate.vue
@@ -0,0 +1,14 @@
+<template>
+    <review>
+         <h4>{{ review.description }}</h4>
+         <b> Rating: {{ review.rating }} stars</b>
+         <p>{{ review.date }} by {{  review.username }}</p>
+    </review>
+</template>
+
+<script>
+export default {
+ props: ["review"]
+}
+</script>
+
